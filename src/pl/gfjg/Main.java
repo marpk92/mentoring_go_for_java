673d0b2123c8c4131632cf4095bae37b4c65eff1
Marcin <prymeksoftware@gmail.com>
2023-05-17 16:36:16 +0200

    prosta lista

--- src/pl/gfjg/Main.java
+++ src/pl/gfjg/Main.java
@@ -1,8 +1,29 @@
 package pl.gfjg;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var lenovo = new Product("Lenovo", 2000L);
+        var hp = new Product("HP", 3000L);
+
+        var notebooks = List.of(lenovo, hp);
+
+        for (var notebook : notebooks) {
+            System.out.println(notebook.price);
+        }
+    }
+
+}
+
+class Product {
+    public final String name;
+    public final Long price;
+
+    Product(String name, Long price) {
+        this.name = name;
+        this.price = price;
     }
 }
